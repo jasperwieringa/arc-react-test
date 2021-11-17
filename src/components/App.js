d604d2336760a89dba91996c4149df8344b64ba0
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-11-17 16:07:45 +0100

    Final changes

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,15 @@ function App(props) {
     const { theme } = props;
 
     return (
-        <arc-container id={'container'} theme={theme}>
+        <arc-container theme={theme}>
             <arc-navbar slot={'nav'} logo={'/assets/arc-red.svg'}>
                 <arc-button type={'tab'}>Tab One</arc-button>
             </arc-navbar>
             <arc-sidebar slot={'side'}>
-                <div style={{padding: 'var(--arc-spacing-small)'}}>Some sidebar content</div>
+                <div style={{padding: 'var(--arc-spacing-small)'}}>Sidebar content</div>
             </arc-sidebar>
             <div style={{padding: 'var(--arc-spacing-small)'}}>
-                Some default content
+                Default content
             </div>
         </arc-container>
     );
