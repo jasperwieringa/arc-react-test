21792f16ac9bfad4aabc41b7144013eb56e8f3da
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-02-25 17:08:25 +0100

    Removed unused imports.

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import '@arc-web/components/dist/components/container/arc-container.js';
 import '@arc-web/components/dist/components/navbar/arc-navbar.js';
 import '@arc-web/components/dist/components/sidebar/arc-sidebar.js';
-import '@arc-web/components/dist/components/button/arc-button.js';
-import '@arc-web/components/dist/components/icon-button/arc-icon-button.js';
 
 function App(props) {
     const { theme } = props;
