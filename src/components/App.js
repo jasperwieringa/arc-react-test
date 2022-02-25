733af52b2ec13edc5d73e9f25b7bba485e55cee4
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-02-25 16:59:14 +0100

    Updated the Arc-React demo.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import '@arc-web/components/dist/components/container/arc-container.js';
+import '@arc-web/components/dist/components/navbar/arc-navbar.js';
+import '@arc-web/components/dist/components/sidebar/arc-sidebar.js';
 import '@arc-web/components/dist/components/button/arc-button.js';
 import '@arc-web/components/dist/components/icon-button/arc-icon-button.js';
 
@@ -6,7 +9,17 @@ function App(props) {
     const { theme } = props;
 
     return (
-        <arc-icon-button name={'menu'}>Hello World</arc-icon-button>
+        <arc-container theme={theme}>
+            <arc-navbar slot={'nav'}>
+                <arc-icon-button name={'home'}><></></arc-icon-button>
+            </arc-navbar>
+            <arc-sidebar slot={'side'} title={'Make a selection'}>
+                <div style={{padding: 'var(--arc-spacing-medium'}}>Some sidebar content</div>
+            </arc-sidebar>
+            <section aria-label={'Demo section'} style={{padding: 'var(--arc-spacing-medium'}}>
+                Some content
+            </section>
+        </arc-container>
     );
 }
 
