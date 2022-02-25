352f4e6a1de96b7cde092717657f74a322d7dfd7
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-02-25 13:36:04 +0100

    Tested the new icon approach in React

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,12 @@
-import '@arc-web/components/dist/components/container/arc-container.js';
-import '@arc-web/components/dist/components/navbar/arc-navbar.js';
-import '@arc-web/components/dist/components/sidebar/arc-sidebar.js';
+import React from 'react';
 import '@arc-web/components/dist/components/button/arc-button.js';
+import '@arc-web/components/dist/components/icon-button/arc-icon-button.js';
 
 function App(props) {
     const { theme } = props;
 
     return (
-        <arc-container theme={theme}>
-            <arc-navbar slot={'nav'} logo={'/assets/arc-red.svg'}>
-                <arc-button type={'tab'}>Tab One</arc-button>
-            </arc-navbar>
-            <arc-sidebar slot={'side'}>
-                <div style={{padding: 'var(--arc-spacing-small)'}}>Sidebar content</div>
-            </arc-sidebar>
-            <div style={{padding: 'var(--arc-spacing-small)'}}>
-                Default content
-            </div>
-        </arc-container>
+        <arc-icon-button name={'menu'}>Hello World</arc-icon-button>
     );
 }
 
